5d64bfc33a4206e5847be5e2e6699795706ca959
ttakuma17 <ttanimiya@gmail.com>
2026-05-19 22:34:03 +0900

    infra(tf): ECR repo + lifecycle, ECS cluster + log group

---
+++ infra/terraform/ecr.tf
@@ -0,0 +1,28 @@
+resource "aws_ecr_repository" "app" {
+  name                 = "${var.project}-${var.env}"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images older than 14 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 14
+        }
+        action = { type = "expire" }
+      }
+    ]
+  })
+}
